4e82567ea051b33e4027391bf2f0e67f9d71ca30
Luan Gjokaj <luan@deep-impact.ch>
2017-05-14 16:18:45 +0200

    Cleanup.

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,68 +1,60 @@
 let url = window.location.hash;
 url = url.toString().replace('#', '');
-console.log(Cookies.get());
-
-if (url) {
-    console.log('The url is ' + url);
-}
 
 $(window).on('load', function () {
-    setTimeout(function () {
-        $('.last').addClass('last-loaded');
-    }, 2000);
-    AOS.init();
+	setTimeout(function () {
+		$('.last').addClass('last-loaded');
+	}, 2000);
+	AOS.init();
 });
 
 const langs = ['en', 'it', 'de'];
 const translate = function (jsdata) {
-    $("[tkey]").each(function (index) {
-        var strTr = jsdata [$(this).attr('tkey')];
-        $(this).html(strTr);
-    });
+	$("[tkey]").each(function (index) {
+		var strTr = jsdata [$(this).attr('tkey')];
+		$(this).html(strTr);
+	});
 }
 
 let langCode = navigator.language.substr(0, 2);
 
 if (Cookies.get('lang') === undefined) {
-    Cookies.set('lang', langCode, { expires: 365 }, { path: '' });
+	Cookies.set('lang', langCode, { expires: 365 }, { path: '' });
 }
+
 const cookieLang = Cookies.get('lang');
-console.log(cookieLang);
 
 if (langs.includes(cookieLang))
-    $.getJSON('lang/' + cookieLang + '.json', translate);
+	$.getJSON('lang/' + cookieLang + '.json', translate);
+
 else if (url == 'de') {
-    $.getJSON('lang/de.json', translate);
+	$.getJSON('lang/de.json', translate);
 }
+
 else if (url == 'it') {
-    $.getJSON('lang/it.json', translate);
+	$.getJSON('lang/it.json', translate);
 }
+
 else if (url == 'en') {
-    $.getJSON('lang/en.json', translate);
+	$.getJSON('lang/en.json', translate);
 }
 
 $('#changeEn').on('click', function () {
-    Cookies.set('lang', 'en', { expires: 365 }, { path: '' });
-    window.location.reload();
+	Cookies.set('lang', 'en', { expires: 365 }, { path: '' });
+	window.location.reload();
 })
 
 $('#changeDe').on('click', function () {
-    Cookies.set('lang', 'de', { expires: 365 }, { path: '' });
-    window.location.reload();
+	Cookies.set('lang', 'de', { expires: 365 }, { path: '' });
+	window.location.reload();
 })
 
 $('#changeIt').on('click', function () {
-    Cookies.set('lang', 'it', { expires: 365 }, { path: '' });
-    window.location.reload();
+	Cookies.set('lang', 'it', { expires: 365 }, { path: '' });
+	window.location.reload();
 })
 
 $('#removeLang').on('click', function () {
-    Cookies.remove('lang', { path: '' });
-    window.location.reload();
+	Cookies.remove('lang', { path: '' });
+	window.location.reload();
 })
-
-if (langs.includes(cookieLang)) {
-    console.log('Yes cookieLang is in langs');
-}
-
-console.log(Cookies.get());
