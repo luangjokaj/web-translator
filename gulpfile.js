73b6ec71c4960c232d31d069b37f920b800704f6
Luan Gjokaj <luan@deep-impact.ch>
2017-06-08 21:06:10 +0200

    Added Cache-Busting

--- gulpfile.js
+++ gulpfile.js
@@ -21,6 +21,7 @@ var htmlmin = require('gulp-htmlmin');
 var babel = require("gulp-babel");
 var fileinclude = require('gulp-file-include');
 var modRewrite = require('connect-modrewrite');
+var cachebust = require('gulp-cache-bust');
 //--------------------------------------------------------------------------------------------------
 /* -------------------------------------------------------------------------------------------------
 	PostCSS Plugins
@@ -94,6 +95,9 @@ gulp.task('process-static-files-dev', function () {
 				basepath: '@file'
 			}
 		}))
+		.pipe(cachebust({
+			type: 'timestamp'
+		}))
 		.pipe(gulp.dest('app'));
 });
 
@@ -105,6 +109,9 @@ gulp.task('process-static-files-prod', function () {
 				prefix: '@@',
 				basepath: '@file'
 			}
+		}))
+		.pipe(cachebust({
+			type: 'timestamp'
 		}))
 		.pipe(htmlmin({
 			collapseWhitespace: true,
